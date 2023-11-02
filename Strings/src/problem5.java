0b7f8c551e7358f887114db9f1caf3f708b7529a
Bhargav200004 <Bhargav200004@gmail.com>
2023-11-02 23:22:22 +0530

    updating

--- Strings/src/problem5.java
+++ Strings/src/problem5.java
@@ -2,20 +2,35 @@ public class problem5 {
     public static void main(String[] args) {
         String st = "welcometoscaler";
 
+        char [] ch = st.toCharArray();
+
         int i = 0 ;
-        int j = st.length() -1;
+        int j = ch.length -1;
 
-        while(i > j){
-            if (st.charAt(i) == 'a' || st.charAt(i) == 'e' || st.charAt(i) == 'i' || st.charAt(i) == 'o' || st.charAt(i) == 'u' || st.charAt(i) == 's'){
-                j--;
-                if (st.charAt(j) == 'a' || st.charAt(j) == 'e' || st.charAt(j) == 'i' || st.charAt(j) == 'o' || st.charAt(j) == 'u' || st.charAt(j) == 's'){
-                    char temp = st.charAt(i);
-                    st.charAt(i) = st.charAt(j);
-                    st.charAt(j) = temp;
+        while(i < j){
 
-                    i++;
-                }
+            while(ch[i]!='a' && ch[i]!='e' && ch[i]!='i' && ch[i]!='o' && ch[i]!='u' && ch[i]!='s'){
+                i++;
             }
+
+            while(ch[j]!='a' && ch[j]!='e' && ch[j]!='i' && ch[j]!='o' && ch[j]!='u' && ch[j]!='s'){
+                j--;
+            }
+
+
+            if (i > j ) break;
+
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        System.out.println("After complete");
+        for (char c : ch){
+            System.out.print(c);
         }
     }
 
