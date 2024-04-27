6f74f2cf0ef624a0ec23584d4fe14457c446591e
Bhargav200004 <Bhargav200004@gmail.com>
2024-04-27 12:29:06 +0530

    problem next greater element

---
+++ Stack/src/Problem4.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+public class Problem4 {
+
+    static int [] nextGreater(int [] arr){
+        int n = arr.length;
+        int []res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        res[n-1] = -1;
+        st.push(arr[n-1]);
+        for (int i = n-2 ; i >= 0 ; i--){
+            if (st.isEmpty()){
+                st.push(arr[i]);
+            }
+            else if(arr[i] < st.peek()){
+                res[i]= st.peek();
+                st.push(arr[i]);
+            }
+            else{
+                while(!st.isEmpty() &&  arr[i ] > st.peek() ){
+                    st.pop();
+                }
+                if (st.isEmpty()){
+                    st.push(arr[i]);
+                    res[i] = -1;
+                }
+                else{
+                    res[i]= st.peek();
+                    st.push(arr[i]);
+                }
+            }
+
+        }
+
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr = {1,3,2,1,8,6,3,4};
+        int [] res = nextGreater(arr);
+
+        for (int i : res){
+            System.out.print(i+" ");
+        }
+
+    }
+
+
+}
