3b0160d04409499c39cc447c1d42341be5797ce0
Bhargav200004 <Bhargav200004@gmail.com>
2024-05-25 00:30:38 +0530

    mapInterface java collection

---
+++ JavaCollectionFrameWork/src/MapInterfaceExample.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class MapInterfaceExample {
+    public static void main(String[] args) {
+        HashMap<Integer , String> hashMap = new HashMap<>();
+        hashMap.put(3,"Bhargav");
+        hashMap.put(1,"meghana");
+        hashMap.put(2,"anshu");
+        hashMap.put(4,"raghav");
+        hashMap.put(1,"vivek"); // override by new value
+        hashMap.putIfAbsent(1,"sourik");
+
+        //iterating over map
+
+        for (var i : hashMap.values()){
+            if ("Bhargav" == i) System.out.print(i+" ");
+        }
+
+
+//        System.out.println(hashMap.keySet());
+//        System.out.println(hashMap.get(3));
+//        System.out.println(hashMap.containsKey(5));
+//        System.out.println(hashMap.containsValue("Bhargav"));
+        System.out.println(hashMap.values());
+    }
+}
