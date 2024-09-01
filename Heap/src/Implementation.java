4829e5374a5c14c332d1a104a0d5803f1f113e19
Bhargav200004 <Bhargav200004@gmail.com>
2024-09-01 09:48:29 +0530

    Implementation of heap

---
+++ Heap/src/Implementation.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Implementation {
+
+    static void heapyfy(List<Integer> heap , int index){
+        int parent = (index - 1) / 2;
+        if (index == 0 || heap.get(parent) < heap.get(index)) return;
+        int temp = heap.get(parent);
+        heap.set(parent , heap.get(index));
+        heap.set(index , temp);
+        heapyfy(heap , parent);
+    };
+
+    static void insertInMinHeap(List<Integer> heap , int element){
+        heap.add(element);
+
+        int index = heap.size() - 1;
+
+        heapyfy(heap , index);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> heap = new ArrayList<>();
+        heap.add(10);
+        heap.add(20);
+        heap.add(30);
+        heap.add(40);
+        heap.add(50);
+
+        System.out.println(heap);
+        insertInMinHeap(heap , 434);
+        System.out.println(heap);
+    }
+}
